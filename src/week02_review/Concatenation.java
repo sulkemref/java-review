b0c1758eea61fa36d4b24f2f0ee20104245ad982
Kemail-terminal <sulkemref@gmail.com>
2022-09-10 20:04:51 -0500

    week02_review_Concatenation

---
+++ src/week02_review/Concatenation.java
@@ -0,0 +1,42 @@
+package week02_review;
+
+public class Concatenation {
+
+    public static void main(String[] args) {
+
+        String name = "Ana";
+
+        int age = 25;
+
+        String companyName = "Bank of America";
+        double salary = 85000.5;
+
+/*
+        name = "Kudret";
+
+        age = 28;
+
+        companyName = "Microsoft";
+
+        salary = 100000;
+*/
+
+        System.out.println("Hello " +name);
+
+        System.out.println(name + " is " + age +  " years old");
+
+        System.out.println(name + " works at " + companyName  + " and makes " + salary + " dollars per year" );
+
+
+
+       // String a, b, c, d;
+
+
+
+
+
+
+
+    }
+
+}
