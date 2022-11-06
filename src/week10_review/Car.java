b3ee89d0f05d398c890cb9d3d869f84d80eb060b
Kemail-terminal <sulkemref@gmail.com>
2022-11-05 20:35:54 -0500

    week10_review_Car

---
+++ src/week10_review/Car.java
@@ -0,0 +1,35 @@
+package week10_review;
+
+public class Car {
+
+    public String make, model, color;
+    public int year;
+    public double price;
+
+
+    public class Tire{
+        public String brand;
+        public String size;
+        public double price;
+        //public static int number =4;
+
+        public void m(){
+           // System.out.println(make);
+           // System.out.println(model);
+        }
+
+
+    }
+
+}
+
+
+
+class Q{
+    public static void main(String[] args) {
+
+      // System.out.println( Car.Tire.number );
+
+    }
+
+}
